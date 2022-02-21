bfd112e7b3c73cc9cfda5ce151fec793a6a75aa8
armenius777 <44444282+armenius777@users.noreply.github.com>
2022-02-22 00:06:41 +0400

    Swipe to left is not working
    try to delete saved publication with swipe to left, but swipe isn't worked.

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,9 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -20,7 +22,15 @@ public class TestClass {
     public static final String SEARCH_FIELD_ID = "org.wikipedia:id/search_src_text";
     public static final String SEARCH_RESULTS_AREA_ID = "org.wikipedia:id/search_results_list";
     public static final String CLEAR_SEARCH_BUTTON_ID = "org.wikipedia:id/search_close_btn";
+    public static final String BACK_TO_HOME_BUTTON_XPATH = "//android.view.ViewGroup[@resource-id=\"org.wikipedia:id/search_toolbar\"]//android.widget.ImageButton";
     public static final String RESULTS_TITLE_ID = "org.wikipedia:id/page_list_item_title";
+    public static final String HEADER_IMAGE_ID = "org.wikipedia:id/view_page_header_image";
+    public static final String SAVE_BUTTON_ID = "org.wikipedia:id/article_menu_bookmark";
+    public static final String BACK_TO_SEARCH_BUTTON_XPATH = "//android.widget.ImageButton[@content-desc=\"Navigate up\"]";
+    public static final String SAVED_BUTTON_XPATH = "//android.widget.FrameLayout[@content-desc=\"Saved\"]";
+    public static final String SAVED_PUBLICATIONS_FOLDER_ID = "org.wikipedia:id/recycler_view";
+    public static final String SAVED_PUBLICATIONS_LIST_XPATH = "//androidx.recyclerview.widget.RecyclerView[@resource-id=\"org.wikipedia:id/reading_list_recycler_view\"]/android.view.ViewGroup";
+
 
     //Texts
     public static final String NEW_WAYS_TEXT = "New ways to explore";
@@ -47,54 +57,93 @@ public class TestClass {
         driver.quit();
     }
 
-    @Test
-    public void firstTest() {
-        WebElement continueButton =
-                waitForElementPresented(By.id(CONTINUE_BUTTON_ID), 3, "Continue button is missing");
-        continueButton.click();
-        WebElement newWays =
-                waitForElementPresented(By.id(NEW_WAYS_ID), 5, "New ways text is missing");
-        assertElementHasText(newWays, NEW_WAYS_TEXT, "New ways text is incorrect");
-        skipIntro();
-        WebElement homepageSearchField =
-                waitForElementPresented(By.xpath(HOMEPAGE_SEARCH_FIELD_XPATH),
-                        5, "Search field is missing in the HomePage");
-        assertElementHasText(homepageSearchField, SEARCH_FIELD_PLACEHOLDER,
-                "Homepage Search field text is incorrect");
-        homepageSearchField.click();
-        WebElement searchField =
-                waitForElementPresented(By.id(SEARCH_FIELD_ID), 5, "Search field is missing");
-        assertElementHasText(searchField, SEARCH_FIELD_PLACEHOLDER, "Search field text is incorrect");
-    }
+//    @Test
+//    public void firstTest() {
+//        WebElement continueButton =
+//                waitForElementPresented(By.id(CONTINUE_BUTTON_ID), 3, "Continue button is missing");
+//        continueButton.click();
+//        WebElement newWays =
+//                waitForElementPresented(By.id(NEW_WAYS_ID), 5, "New ways text is missing");
+//        assertElementHasText(newWays, NEW_WAYS_TEXT, "New ways text is incorrect");
+//        skipIntro();
+//        WebElement homepageSearchField =
+//                waitForElementPresented(By.xpath(HOMEPAGE_SEARCH_FIELD_XPATH),
+//                        5, "Search field is missing in the HomePage");
+//        assertElementHasText(homepageSearchField, SEARCH_FIELD_PLACEHOLDER,
+//                "Homepage Search field text is incorrect");
+//        homepageSearchField.click();
+//        WebElement searchField =
+//                waitForElementPresented(By.id(SEARCH_FIELD_ID), 5, "Search field is missing");
+//        assertElementHasText(searchField, SEARCH_FIELD_PLACEHOLDER, "Search field text is incorrect");
+//    }
+//
+//    @Test
+//    public void secondTest() {
+//        skipIntro();
+//        WebElement homepageSearchField =
+//                waitForElementPresented(By.xpath(HOMEPAGE_SEARCH_FIELD_XPATH),
+//                        5, "Search field is missing in the HomePage");
+//        homepageSearchField.click();
+//        WebElement searchField =
+//                waitForElementPresented(By.id(SEARCH_FIELD_ID), 5, "Search field is missing");
+//        searchField.sendKeys("Java");
+//        WebElement searchResultsArea =
+//                waitForElementPresented(By.id(SEARCH_RESULTS_AREA_ID), 5, "Search result is empty");
+//        WebElement searchCloseBtn =
+//                waitForElementPresented(By.id(CLEAR_SEARCH_BUTTON_ID), 5, "Close button is missing");
+//        searchCloseBtn.click();
+//        Assert.assertFalse("Search not canceled", searchResultsArea.isDisplayed());
+//    }
+
+//    @Test
+//    public void thirdTest() {
+//        skipIntro();
+//        searchWord(SEARCH_WORD);
+//        waitForElementsPresented(By.id(RESULTS_TITLE_ID), 10, "Search result is empty");
+//        List<WebElement> resultsList = driver.findElements(By.id(RESULTS_TITLE_ID));
+//        for (WebElement result : resultsList) {
+//            Assert.assertTrue("Search Result is incorrect",
+//                    result.getText().toLowerCase().contains(SEARCH_WORD.toLowerCase()));
+//        }
+//    }
 
     @Test
-    public void secondTest() {
-        skipIntro();
-        WebElement homepageSearchField =
-                waitForElementPresented(By.xpath(HOMEPAGE_SEARCH_FIELD_XPATH),
-                        5, "Search field is missing in the HomePage");
-        homepageSearchField.click();
-        WebElement searchField =
-                waitForElementPresented(By.id(SEARCH_FIELD_ID), 5, "Search field is missing");
-        searchField.sendKeys("Java");
-        WebElement searchResultsArea =
-                waitForElementPresented(By.id(SEARCH_RESULTS_AREA_ID), 5, "Search result is empty");
-        WebElement searchCloseBtn =
-                waitForElementPresented(By.id(CLEAR_SEARCH_BUTTON_ID), 5, "Close button is missing");
-        searchCloseBtn.click();
-        Assert.assertFalse("Search not canceled", searchResultsArea.isDisplayed());
-    }
-
-    @Test
-    public void thirdTest() {
+    public void testSavePublicationsAndRemove() throws InterruptedException {
         skipIntro();
         searchWord(SEARCH_WORD);
         waitForElementsPresented(By.id(RESULTS_TITLE_ID), 10, "Search result is empty");
         List<WebElement> resultsList = driver.findElements(By.id(RESULTS_TITLE_ID));
-        for (WebElement result : resultsList) {
-            Assert.assertTrue("Search Result is incorrect",
-                    result.getText().toLowerCase().contains(SEARCH_WORD.toLowerCase()));
-        }
+        Assert.assertTrue("Search result is empty", resultsList.size() > 0);
+        resultsList.get(1).click();
+        waitForElementPresented(By.id(HEADER_IMAGE_ID), 10, "Header image is not shown");
+        WebElement saveButton = waitForElementPresented(By.id(SAVE_BUTTON_ID), 10, "Save button is missing");
+        saveButton.click();
+        WebElement backToSearch = waitForElementPresented(By.xpath(BACK_TO_SEARCH_BUTTON_XPATH), 10,
+                "Back to search arrow-button is missing");
+        backToSearch.click();
+        resultsList = driver.findElements(By.id(RESULTS_TITLE_ID));
+        resultsList.get(2).click();
+        waitForElementPresented(By.id(HEADER_IMAGE_ID), 10, "Header image is not shown");
+        saveButton = waitForElementPresented(By.id(SAVE_BUTTON_ID), 10, "Save button is missing");
+        saveButton.click();
+        backToSearch = waitForElementPresented(By.xpath(BACK_TO_SEARCH_BUTTON_XPATH), 10,
+                "Back to search arrow-button is missing");
+        backToSearch.click();
+        WebElement backToHome = waitForElementPresented(By.xpath(BACK_TO_HOME_BUTTON_XPATH), 10,
+                "Back to home button is missing");
+        backToHome.click();
+        WebElement savedPublicationsPageButton = waitForElementPresented(By.xpath(SAVED_BUTTON_XPATH), 10,
+                "Saved button is missing");
+        savedPublicationsPageButton.click();
+        WebElement savedPublicationsFolder = waitForElementPresented(By.id(SAVED_PUBLICATIONS_FOLDER_ID), 10,
+                "Saved publications folder is missing");
+        savedPublicationsFolder.click();
+        List<WebElement> savedPublications = waitForElementsPresented(By.xpath(SAVED_PUBLICATIONS_LIST_XPATH), 10,
+                "Saved publications are missing");
+        Assert.assertTrue("", savedPublications.size() > 2);
+        swipeElementToLeft(savedPublications.get(1));
+        Assert.assertTrue("", savedPublications.size() <= 2);
+        Thread.sleep(10000);
     }
 
 
@@ -105,10 +154,16 @@ public class TestClass {
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
-    private void waitForElementsPresented(By by, long timeout, String errorMessage) {
+    private List<WebElement> waitForElementsPresented(By by, long timeout, String errorMessage) {
         WebDriverWait wait = new WebDriverWait(driver, timeout);
         wait.withMessage(errorMessage);
-        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(by, 0));
+        return wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(by, 0));
+    }
+
+    private WebElement waitForElementAndClear(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresented(by,timeoutInSeconds, errorMessage);
+        element.clear();
+        return element;
     }
 
 
@@ -139,6 +194,59 @@ public class TestClass {
                 waitForElementPresented(By.id(SEARCH_FIELD_ID), 5, "Search field is missing");
         searchField.sendKeys(word);
         waitForElementPresented(By.id(SEARCH_RESULTS_AREA_ID), 5, "Search result is empty");
+    }
+
+    private void swipeUp(int timeOfSwipe) {
+        TouchAction action = new TouchAction(driver);
+        Dimension screenSize = driver.manage().window().getSize();
+        int x = screenSize.width / 2;
+        int startY = (int) (screenSize.height * 0.8);
+        int endY = (int) (screenSize.height * 0.2);
+        action
+                .press(x, startY)
+                .waitAction(timeOfSwipe)
+                .moveTo(x, endY)
+                .release()
+                .perform();
+    }
+
+    private void quickSwipeUp() {
+        swipeUp(200);
+    }
+
+    private void swipeUpToElement(By by, int maxSwipes, String errorMessage) {
+        int currentSwipes = 0;
+        while (driver.findElements(by).size() == 0) {
+            if (currentSwipes > maxSwipes) {
+                waitForElementPresented(by, 0, "Element not found after all swipes");
+                return;
+            }
+            quickSwipeUp();
+            ++currentSwipes;
+        }
+    }
+
+    private void swipeElementToLeft(By by, String errorMessage) {
+        WebElement element = waitForElementPresented(by, 10, errorMessage);
+        swipeElementToLeft(element);
+    }
+
+    private void swipeElementToLeft(WebElement element) {
+        int leftX = element.getLocation().getX()+10;
+        int rightX = leftX + element.getSize().getWidth()-10;
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();
+        int middleY = (upperY + lowerY) / 2;
+
+        TouchAction action = new TouchAction(driver);
+//        action.tap(rightX, middleY).perform();
+        //TODO this actions isn't worked
+        action
+                .press(rightX, middleY)
+                .waitAction(500)
+                .moveTo(leftX, middleY)
+                .release()
+                .perform();
     }
 
 }
